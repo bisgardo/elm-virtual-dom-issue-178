a11fe7bf8347a2d85e48351b35fd81ae96899e5d
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-09-22 00:01:50 +0200

    Randomize insertions

--- src/Main.elm
+++ src/Main.elm
@@ -3,20 +3,39 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (..)
-import Html.Keyed
+import Html.Keyed exposing (..)
+import List.Extra exposing (..)
+import Random exposing (Seed, initialSeed, step)
 
 
-main : Program () Int Int
+main : Program () (List Int) Int
 main =
     Browser.sandbox
-        { init = 0
+        { init = []
         , view = view
-        , update = (+)
+        , update = update
         }
 
 
-view : Int -> Html Int
-view rowCount =
+rand : Int -> Int
+rand limit =
+    step (Random.int 0 limit) (initialSeed limit) |> Tuple.first
+
+
+update : Int -> List Int -> List Int
+update inc keys =
+    let
+        len =
+            List.length keys
+
+        ( l, r ) =
+            splitAt (rand len) keys
+    in
+    l ++ (List.range 1 inc |> List.map ((+) len)) ++ r
+
+
+view : List Int -> Html Int
+view keys =
     div []
         [ h1 [] [ text "Elements flash in green when they're attached to the DOM" ]
         , text "When more than one row is added at a time, the final row is redundantly detached and reattached, causing it to lose focus."
@@ -24,7 +43,7 @@ view rowCount =
             []
             (List.concat
                 [ [ ( "first", tr [] [ td [] <| [ text "key=first" ] ++ buttons ] ) ]
-                , List.range 0 rowCount
+                , keys
                     |> List.map String.fromInt
                     |> List.map (\key -> ( key, tr [] [ td [] [ text <| "key=" ++ key ] ] ))
                 , [ ( "last", tr [] [ td [] <| [ text "key=last" ] ++ buttons ] ) ]
