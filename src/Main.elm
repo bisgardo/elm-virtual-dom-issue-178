b56acfedb49e48d8b69b829d6db000781dfb9dd3
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-09-21 23:28:06 +0200

    Work

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,6 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.Keyed
 
@@ -18,11 +17,23 @@ main =
 
 view : Int -> Html Int
 view rowCount =
-    Html.Keyed.node "table"
-        []
-        (List.concat
-            [ [ ( "first", tr [] [ td [] [ button [ onClick 1 ] [ text "Add 1 row" ], button [ onClick 2 ] [ text "Add 2 rows" ] ] ] ) ]
-            , List.range 0 rowCount |> List.map (\key -> ( String.fromInt key, tr [] [ td [] [] ] ))
-            , [ ( "last", tr [] [ td [] [ button [ onClick 1 ] [ text "Add 1 row" ], button [ onClick 2 ] [ text "Add 2 rows" ] ] ] ) ]
-            ]
-        )
+    div []
+        [ h1 [] [ text "Elements flash in green when they're attached to the DOM" ]
+        , text "When more than one row is added at a time, the final row is redundantly detached and reattached, causing it to lose focus."
+        , Html.Keyed.node "table"
+            []
+            (List.concat
+                [ [ ( "first", tr [] [ td [] <| [ text "key=first" ] ++ buttons ] ) ]
+                , List.range 0 rowCount
+                    |> List.map String.fromInt
+                    |> List.map (\key -> ( key, tr [] [ td [] [ text <| "key=" ++ key ] ] ))
+                , [ ( "last", tr [] [ td [] <| [ text "key=last" ] ++ buttons ] ) ]
+                ]
+            )
+        ]
+
+
+buttons =
+    [ button [ onClick 1 ] [ text "Add 1 row" ]
+    , button [ onClick 2 ] [ text "Add 2 rows" ]
+    ]
