5c0e05b99c9aa2fb8af8ada64b1075e183620911
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-09-21 23:10:52 +0200

    Work

--- src/Main.elm
+++ src/Main.elm
@@ -7,29 +7,22 @@ import Html.Events exposing (..)
 import Html.Keyed
 
 
-main : Program () Int ()
+main : Program () Int Int
 main =
     Browser.sandbox
         { init = 0
         , view = view
-        , update = always ((+) 2)
+        , update = (+)
         }
 
 
-update : () -> Int -> Int
-update msg model =
-    case msg of
-        () ->
-            model + 1
-
-
-view : Int -> Html ()
+view : Int -> Html Int
 view rowCount =
     Html.Keyed.node "table"
         []
         (List.concat
-            [ [ ( "first", tr [] [ td [] [ button [ onClick () ] [ text "Add row" ] ] ] ) ]
+            [ [ ( "first", tr [] [ td [] [ button [ onClick 1 ] [ text "Add 1 row" ], button [ onClick 2 ] [ text "Add 2 rows" ] ] ] ) ]
             , List.range 0 rowCount |> List.map (\key -> ( String.fromInt key, tr [] [ td [] [] ] ))
-            , [ ( "last", tr [] [ td [] [ button [ onClick () ] [ text "Add row" ] ] ] ) ]
+            , [ ( "last", tr [] [ td [] [ button [ onClick 1 ] [ text "Add 1 row" ], button [ onClick 2 ] [ text "Add 2 rows" ] ] ] ) ]
             ]
         )
