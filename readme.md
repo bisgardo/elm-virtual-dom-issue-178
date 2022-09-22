11d0f8cb23e360b0536d31b0bbd0fe306b12bd7e
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-09-22 21:19:26 +0200

    Cleanup and documentation

---
+++ readme.md
@@ -0,0 +1,5 @@
+# `elm-virtual-dom-issue-178`
+
+Example project for demonstrating bug [elm/virtual-dom#178](https://github.com/elm/virtual-dom/issues/178).
+
+Online with explanations [here](https://bisgardo.github.io/elm-virtual-dom-issue-178/).
--- src/Main.elm
+++ src/Main.elm
@@ -9,9 +9,12 @@ import List.Extra exposing (..)
 import Random exposing (..)
 
 
-type InsertLocation = Middle | Random
+type InsertLocation
+    = Middle
+    | Random
 
-main : Program () (List Int) (Int, InsertLocation)
+
+main : Program () (List Int) ( Int, InsertLocation )
 main =
     Browser.sandbox
         { init = []
@@ -20,19 +23,18 @@ main =
         }
 
 
-
 splitIndex : InsertLocation -> Int -> Int
 splitIndex loc limit =
     case loc of
-        Middle -> limit // 2
+        Middle ->
+            limit // 2
+
         Random ->
             step (Random.int 0 limit) (initialSeed limit) |> Tuple.first
 
 
-
-
-update : (Int, InsertLocation) -> List Int -> List Int
-update (inc, loc) keys =
+update : ( Int, InsertLocation ) -> List Int -> List Int
+update ( inc, loc ) keys =
     let
         len =
             List.length keys
@@ -50,29 +52,35 @@ update (inc, loc) keys =
             keys
 
 
-view : List Int -> Html (Int, InsertLocation)
+view : List Int -> Html ( Int, InsertLocation )
 view keys =
     div []
-        [ h1 [] [text "Demonstration of ", a [href "https://github.com/elm/virtual-dom/issues/178"] [text "elm/virtual-dom#178"]]
+        [ h1 []
+            [ text "Demonstration of "
+            , a [ href "https://github.com/elm/virtual-dom/issues/178" ] [ text "elm/virtual-dom#178" ]
+            , text " ("
+            , a [ href "https://github.com/bisgardo/elm-virtual-dom-issue-178" ] [ text "source" ]
+            , text ")"
+            ]
         , p [] [ text "Elements flash in green when they're attached to the DOM." ]
         , p [] [ text "When more than one consecutive row is added at a time, then all rows following them are redundantly detached and reattached." ]
         , p [] [ text "If a button on the last row had focus, it will lose it. This can be seen by clicking the button. Pressing ENTER will then \"click\" it again as long as it retains focus." ]
         , Html.Keyed.node "table"
             []
             (List.concat
-                [ [ ( "first", tr [] [ td [] <| [ text "key=first" ] ++ buttons ] ) ]
+                [ [ ( "first", tr [] [ td [] <| [ text "key=first " ] ++ buttons ] ) ]
                 , keys
                     |> List.map String.fromInt
-                    |> List.map (\key -> ( key, tr [] [ td [] [ text <| "key=" ++ key ] ] ))
-                , [ ( "last", tr [] [ td [] <| [ text "key=last" ] ++ buttons ] ) ]
+                    |> List.map (\key -> ( key, tr [] [ td [] [ text <| "key=" ++ key ++ " " ] ] ))
+                , [ ( "last", tr [] [ td [] <| [ text "key=last " ] ++ buttons ] ) ]
                 ]
             )
         ]
 
 
 buttons =
-    [ button [ onClick (1, Middle) ] [ text "Add 1 row (middle)" ]
-    , button [ onClick (2, Middle) ] [ text "Add 2 rows (middle)" ]
-    , button [ onClick (1, Random) ] [ text "Add 1 row (randomly)" ]
-    , button [ onClick (2, Random) ] [ text "Add 2 rows (randomly)" ]
+    [ button [ onClick ( 1, Middle ) ] [ text "Add 1 row (middle)" ]
+    , button [ onClick ( 2, Middle ) ] [ text "Add 2 rows (middle)" ]
+    , button [ onClick ( 1, Random ) ] [ text "Add 1 row (randomly)" ]
+    , button [ onClick ( 2, Random ) ] [ text "Add 2 rows (randomly)" ]
     ]
